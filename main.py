a4ecb0c4348af85e0291ec6242c304ad8beca27d
osm1nozhek <snurenkoviktoria@gmail.com>
2023-04-13 22:45:36 +0300

    .

--- main.py
+++ main.py
@@ -1,13 +1,47 @@
-def parse(query: str) -> dict:
-    return {}
+from function import Parse
 
+import unittest
 
-if __name__ == '__main__':
-    assert parse('https://example.com/path/to/page?name=ferret&color=purple') == {'name': 'ferret', 'color': 'purple'}
-    assert parse('https://example.com/path/to/page?name=ferret&color=purple&') == {'name': 'ferret', 'color': 'purple'}
-    assert parse('http://example.com/') == {}
-    assert parse('http://example.com/?') == {}
-    assert parse('http://example.com/?name=Dima') == {'name': 'Dima'}
+class TestParse(unittest.TestCase):
+    def test_name(self):
+        res = Parse('https://example.com/path/to/page?name=ferret')
+        self.assertEqual(res, {'name': 'ferret'})
+
+    def test_color(self):
+        res = Parse('https://example.com/path/to/page?color=purple')
+        self.assertEqual(res, {'color': 'purple'})
+
+    def test_age(self):
+        res = Parse('https://example.com/path/to/page?age=18')
+        self.assertEqual(res, {'age': '18'})
+
+    def test_empty1(self):
+        res = Parse('http://example.com/')
+        self.assertEqual(res, {})
+
+    def test_empty2(self):
+        res = Parse('http://example.com/?')
+        self.assertEqual(res, {})
+
+    def test_combination1(self):
+        res = Parse('https://example.com/path/to/page?name=ferret&age=18')
+        self.assertEqual(res, {'name': 'ferret', 'age': '18'})
+
+    def test_combination2(self):
+        res = Parse('https://example.com/path/to/page?name=ferret&color=purple')
+        self.assertEqual(res, {'name': 'ferret', 'color': 'purple'})
+
+    def test_combination3(self):
+        res = Parse('https://example.com/path/to/page?color=purple&age=18')
+        self.assertEqual(res, {'color': 'purple', 'age': '18'})
+
+    def test_combination4(self):
+        res = Parse('https://example.com/path/to/page?name=ferret&color=purple&age=18')
+        self.assertEqual(res, {'name': 'ferret', 'color': 'purple', 'age': '18'})
+
+    def test_combination5(self):
+        res = Parse('https://example.com/path/to/page?name=ferret&color=purple&age=18&')
+        self.assertEqual(res, {'name': 'ferret', 'color': 'purple', 'age': '18'})
 
 
 def parse_cookie(query: str) -> dict:
