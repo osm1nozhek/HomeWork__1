9e0424e902a38bfceca9003566b30968f54a701d
osm1nozhek <snurenkoviktoria@gmail.com>
2023-04-13 22:49:37 +0300

    .

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from function import Parse
+from function import Parse, parse_cookie
 
 import unittest
 
@@ -44,12 +44,48 @@ class TestParse(unittest.TestCase):
         self.assertEqual(res, {'name': 'ferret', 'color': 'purple', 'age': '18'})
 
 
-def parse_cookie(query: str) -> dict:
-    return {}
+class TestParseCookie(unittest.TestCase):
+    def test_name(self):
+        res=parse_cookie('name=Dima;')
+        self.assertEqual(res, {'name': 'Dima'} )
+
+    def test_double_name(self):
+        res = parse_cookie('name=Dima=User;')
+        self.assertEqual(res, {'name': 'Dima=User'})
+
+    def test_age(self):
+        res=parse_cookie('age=28;')
+        self.assertEqual(res, {'age': '28'} )
+
+    def test_colour(self):
+        res=parse_cookie('colour=blue;')
+        self.assertEqual(res, {'colour': 'blue'} )
+
+    def test_empty1(self):
+        res=parse_cookie(' ')
+        self.assertEqual(res, {} )
+
+    def test_ua(self):
+        res=parse_cookie('колір=blue;')
+        self.assertEqual(res, {'колір': 'blue'} )
+
+    def test_combination1(self):
+        res=parse_cookie('name=Dima;age=28;')
+        self.assertEqual(res, {'name': 'Dima', 'age': '28'} )
+
+    def test_combination2(self):
+        res=parse_cookie('name=Dima=User;age=28;')
+        self.assertEqual(res, {'name': 'Dima=User', 'age': '28'} )
+
+    def test_combination3(self):
+        res = parse_cookie('age=28;colour=blue;')
+        self.assertEqual(res, {'age': '28', 'colour': 'blue'})
+
+
+    def test_combination4(self):
+        res=parse_cookie('name=Dima;age=28;colour=blue;')
+        self.assertEqual(res, {'name': 'Dima', 'age': '28','colour': 'blue'})
 
 
 if __name__ == '__main__':
-    assert parse_cookie('name=Dima;') == {'name': 'Dima'}
-    assert parse_cookie('') == {}
-    assert parse_cookie('name=Dima;age=28;') == {'name': 'Dima', 'age': '28'}
-    assert parse_cookie('name=Dima=User;age=28;') == {'name': 'Dima=User', 'age': '28'}
+    unittest.main()
